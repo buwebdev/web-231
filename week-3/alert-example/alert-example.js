2a845777bf7296bd2b4047a9675e42c1f971f4d7
Richard Krasso <rkrasso@bellevue.edu>
2023-03-27 12:08:52 -0500

    added code examples for week three.

---
+++ week-3/alert-example/alert-example.js
@@ -0,0 +1,18 @@
+/*
+============================================
+; Title:  alert-examples.js 
+; Author: Professor Krasso
+; Date:   27 March 2023 
+; Description: Examples demonstrating how to 
+;   register onclick events and displaying
+;   alert messages. 
+;===========================================
+*/
+
+function favoriteFood () {
+    alert('My favorite food is sushi!')
+}
+
+function favoriteColor () {
+    alert('My favorite color is red!')
+}
