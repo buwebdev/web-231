78a37899cbfc959b316e471aa4008fd3690b6ca2
Richard Krasso <rkrasso@bellevue.edu>
2023-04-20 14:05:00 -0500

    updated week 7 example code.

--- week-7/ex02.js
+++ week-7/ex02.js
@@ -7,9 +7,16 @@
  * @returns {string} Returns full name
  */
 function myName (firstName, lastName) {
-  if (typeof firstName !== 'string' || typeof lastName !== 'string') {
-    throw Error('inputs must be strings') // Throw a new error
-  } // end of if block
+  const fname = parseFloat(firstName)
+  const lname = parseFloat(lastName)
+
+  if (!isNaN(fname) || !isNaN(lname)) {
+    throw Error('inputs must be strings')
+  } // end if block.
+
+  if (firstName.length === 0 || lastName.length === 0) {
+    throw Error('inputs are required')
+  } // end if block.
 
   return firstName + ' ' + lastName
 } // end of myName function
